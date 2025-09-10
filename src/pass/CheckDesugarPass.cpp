340186f0103d9a535cca2cbdea5141d4c2c0d1eb
tinychong <1320765753@qq.com>
2025-09-10 23:22:21 +0800

    test(desugar): 添加生成 golden data 功能并优化测试流程
    - 在 CI 测试中添加 GenGolden 测试用例，用于生成 golden data
    - 修改测试配置，支持 desugar 阶段的测试
    - 增加 MoveFile 函数，用于移动生成的 golden data 文件
    - 更新相关头文件和源文件，支持表达式反糖功能

--- src/pass/CheckDesugarPass.cpp
+++ src/pass/CheckDesugarPass.cpp
@@ -12,6 +12,9 @@ CheckDesugarPass::CheckDesugarPass(const PassConfig& config) : Pass(config)
 {
     visitor.RegVisit(
         AstKind::OPTION_TYPE, [this](AstNode& node, ValuedResult& res) { Visit(Cast<OptionType&>(node), res); });
+
+    visitor.RegVisit(
+        AstKind::TRAIL_CLOSURE_EXPR, [this](AstNode& node, ValuedResult& res) { Visit(Cast<Expr&>(node), res); });
 }
 
 void CheckDesugarPass::Run(AstNode& node)
@@ -25,6 +28,14 @@ void CheckDesugarPass::Visit(OptionType& node, ValuedResult& res)
 {
     DEBUG("OptionType");
     if (node.desugarType) {
+        res.Set<DefaultValue>(1);
+    }
+}
+
+void CheckDesugarPass::Visit(Expr& node, ValuedResult& res)
+{
+    DEBUG("Expr");
+    if (node.desugarExpr) {
         res.Set<DefaultValue>(1);
     }
 }
--- src/pass/ReplaceDesugarPass.cpp
+++ src/pass/ReplaceDesugarPass.cpp
@@ -12,6 +12,8 @@ ReplaceDesugarPass::ReplaceDesugarPass(const PassConfig& config) : Pass(config)
 {
     visitor.RegVisit(
         AstKind::OPTION_TYPE, [this](AstNode& node, ValuedResult& res) { Visit(Cast<OptionType&>(node), res); });
+    visitor.RegVisit(
+        AstKind::TRAIL_CLOSURE_EXPR, [this](AstNode& node, ValuedResult& res) { Visit(Cast<Expr&>(node), res); });
 }
 
 void ReplaceDesugarPass::Run(AstNode& node)
@@ -24,5 +26,15 @@ void ReplaceDesugarPass::Run(AstNode& node)
 void ReplaceDesugarPass::Visit(OptionType& node, ValuedResult& res)
 {
     DEBUG("OptionType");
-    res.Set<OwnedNodeValue>(std::move(node.desugarType));
+    if (node.desugarType) {
+        res.Set<OwnedNodeValue>(std::move(node.desugarType));
+    }
+}
+
+void ReplaceDesugarPass::Visit(Expr& node, ValuedResult& res)
+{
+    DEBUG("Expr");
+    if (node.desugarExpr) {
+        res.Set<OwnedNodeValue>(std::move(node.desugarExpr));
+    }
 }
